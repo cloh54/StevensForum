38f607f8859f8b9a946d7bdea70225e2da5c40af
albionkaferi <albionkaferi02@gmail.com>
2022-12-16 09:49:34 -0500

    Update reports.js

--- routes/reports.js
+++ routes/reports.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const data = require('../data');
+const reportsData = data.reports;
+
+router.get('/', async (req, res) => {
+    if (req.session.user) {
+        res.redirect('/');
+    }
+    let reportList = await reportsData.getAllReports();
+    res.render('reports', {reportList: reportList});
+});
 
 module.exports = router;
