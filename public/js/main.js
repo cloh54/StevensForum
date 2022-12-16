8946aed3524576b12e368430cf74475561bda9eb
cynth <cynthia.loh1011@gmail.com>
2022-12-16 15:57:49 -0500

    report button and modal form

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,14 @@
 (function () {
-    
+    let reportModal = document.getElementById("report-modal");
+    let reportButton = document.getElementById("report-button");
+    let span = document.getElementsByClassName("close")[0];
+
+    reportButton.onclick = function() {
+        console.log("report button clicked")
+        reportModal.style.display = "block";
+    };
+
+    span.onclick = function() {
+        reportModal.style.display = "none";
+    }
 })();
