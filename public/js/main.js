d986ae1dc89a13a1cbe5e3b1c81aa5953172c815
cynth <cynthia.loh1011@gmail.com>
2022-12-18 23:09:20 -0500

    fix delete comment

--- public/js/main.js
+++ public/js/main.js
@@ -36,16 +36,4 @@
     closeDeletePost.onclick = function() {
         deletePostModal.style.display = "none";
     };
-
-    let deleteCommentModal = document.getElementById("delete-comment-modal");
-    let deleteCommentButton = document.getElementById("delete-comment-button");
-    let closeDeleteComment = document.getElementById("close-delete-comment");
-
-    deleteCommentButton.onclick = function() {
-        deleteCommentModal.style.display = "block";
-    };
-
-    closeDeleteComment.onclick = function() {
-        deleteCommentModal.style.display = "none";
-    };
 })();
